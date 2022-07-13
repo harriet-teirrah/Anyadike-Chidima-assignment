92994df143c51e0c3ab332e2110d2add27de9b78
Anyadike Chidima Harriet <63169473+harriet-teirrah@users.noreply.github.com>
2022-07-13 23:46:15 +0000

    challenge 1 and challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -37,11 +37,12 @@ function sumOfNumbers(arrayOfNumbers) {
 function countEvenNumbers(arrayOfNumbers) {
   count = 0;
   for (let i = 0; i < arrayOfNumbers.length; i++) {
-    if (arrayOfNumbers[i] % 2 == 0){
+    if (arrayOfNumbers[i] % 2 === 0){
       count ++;
+    
     }
   }
-    return arrayOfNumbers.length;
+    return count;
 }
  assignment.countEvenNumbers = countEvenNumbers;
 
